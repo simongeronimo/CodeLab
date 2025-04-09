b6fa084e9d82eaebecae6d087bf10154583a0f1f
simongeronimo <47090861+simongeronimo@users.noreply.github.com>
2025-04-09 17:03:40 -0400

    feat-get available images selection

--- bash/delete-image-from-private-registry.sh
+++ bash/delete-image-from-private-registry.sh
@@ -2,12 +2,27 @@
 
 main(){
     get_registry_address
+    show_available_images
 }
 
 get_registry_address() {
     echo -e "Enter the ip address and port of the private registry (XXX.XX.XX.XXX:PPPPP):"
     read -p "" registry_ip
     echo "You have entered the following address: $registry_ip"
+}
+
+show_available_images() {
+    images=$(curl -X GET $registry_ip/v2/_catalog | jq -r '.repositories[]')
+    echo $images
+    select repo in $images; do
+        if [ -n "$repo" ]; then
+            echo "You selected: $repo"
+            # You can process the selected repository here
+            break
+        else
+            echo "Invalid option, please try again."
+        fi
+    done
 }
 
 main
